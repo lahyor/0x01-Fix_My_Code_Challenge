4ca4cd9ff2c02425db522fff76aa1099a96b99b1
Otekaye Seun <otekayeseun24@gmail.com>
2024-04-16 15:53:37 -0700

    "Need"

--- square.py
+++ square.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
+""" Defines Square class."""
+
 
 class square():
-    
+    """ Square class."""
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ Instantiation of class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Printable representation of square object."""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """ Creates Square object."""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
